5d13ed08a77fd9137c3f8d22fe1ad063631b93e0
Anwar-Barakat <brkatanwar0@gmail.com>
2024-03-27 15:49:13 +0400

    fix

--- app/Mail/SendContactMessageToAdminMail.php
+++ app/Mail/SendContactMessageToAdminMail.php
@@ -11,7 +11,7 @@ use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Address;
 
-class SendContactMessageToAdminMail extends Mailable
+class SendContactMessageToAdminMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
